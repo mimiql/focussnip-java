f30c7846b61dbc7e124dc06f9a0f65aed670d7e3
wuhannan <1242865419@qq.com>
2022-05-16 21:59:44 +0800

    过滤订单模块

--- focussnip_api/src/main/java/org/oss/focussnip/security/ShiroConfig.java
+++ focussnip_api/src/main/java/org/oss/focussnip/security/ShiroConfig.java
@@ -62,6 +62,7 @@ public class ShiroConfig {
         filterRuleMap.put("/test/v2", "anon");
         filterRuleMap.put("/test/**", "jwt");
         filterRuleMap.put("/user", "jwt");
+        filterRuleMap.put("/order/**", "jwt");
         filterRuleMap.put("/shiro/auth/401", "anon");
         // 不拦截swagger
         filterRuleMap.put("/swagger*/**", "anon");
